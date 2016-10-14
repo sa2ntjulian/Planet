da3ed950b247d6cdd344bb89dccc97bf47d2a33d
Ha Jeong-dae <sa2ntjulian@gmail.com>
2016-10-14 23:28:05 +0900

    the second commit.

--- tutorial/src/tutorial/tutorial.java
+++ tutorial/src/tutorial/tutorial.java
@@ -16,7 +16,7 @@ public class tutorial {
 			System.out.println(beerNum + " " + word + " of beer on the wall");
 			System.out.println(beerNum + " " + word + " of beer");
 			System.out.println("Take one down.");
-			System.out.println("Pass it around.");
+			System.out.println("Pass it around~");
 			beerNum = beerNum - 1;
 			
 			if (beerNum > 0) {
