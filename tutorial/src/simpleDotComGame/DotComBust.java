0f44f53692ace53e8d802fc11a4af7e9a76a795b
Ha Jeong-dae <sa2ntjulian@gmail.com>
2016-10-15 20:22:12 +0900

    simpleDotComGame

---
+++ tutorial/src/simpleDotComGame/DotComBust.java
@@ -0,0 +1,38 @@
+package simpleDotComGame;
+
+import java.util.ArrayList;
+
+public class DotComBust {
+	
+	private GameHelper helper = new GameHelper();
+	private ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
+	private numOfGuesses = 0;
+
+	public static void main(String[] args) {
+		DotComBust game = new DotComBust();
+		game.setUpGame();
+		game.startPlaying();
+	}
+	
+	private void setUpGame(){
+		DotCom one = new DotCom();
+		one.setName("Pets.com");
+		DotCom two = new DotCom();
+		two.setName("eToys.com");
+		DotCom three = new DotCom();
+		three.setName("Go2.com");
+		dotComList.add(one);
+		dotComList.add(two);
+		dotComList.add(three);
+		
+		System.out.println("Your goal is to sink three dot com.");
+		System.out.println("Pets.com, eToys.com, Go2.com");
+		System.out.println("Try to sink them all in the fewest number of guesses");
+		
+		for (DotCom dotComToSet : dotComList) {
+			ArrayList<String> newLocation = helper.placeDotCom(3);
+			dotComToSet.setLocationCells(newLocation);
+		}
+	}
+
+}
